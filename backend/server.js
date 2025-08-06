b47d74898f30fe5fa3b8970707779934115acc14
mohitsatyarthiii <mohitsatyarthi11@gmail.com>
2025-08-06 14:23:25 +0530

    update

--- backend/server.js
+++ backend/server.js
@@ -11,9 +11,9 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename)
 
 const app = express();
-const PORT = 4000;
+const PORT = process.env.PORT || 4000;
 
-app.use(cors())
+app.use(cors(origin="*"))
 
 // Connect DB
 connectDB();
